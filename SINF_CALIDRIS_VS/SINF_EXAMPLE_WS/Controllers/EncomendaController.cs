3ce3f4c20d46947b0d1d49760d7bd0e5ad3ce446
MacedoRAC <andr.macedo1@gmail.com>
2015-10-23 18:15:40 +0100

    VS project added

---
+++ SINF_CALIDRIS_VS/SINF_EXAMPLE_WS/Controllers/EncomendaController.cs
@@ -0,0 +1,21 @@
+﻿using SINF_EXAMPLE_WS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SINF_EXAMPLE_WS.Controllers
+{
+    public class EncomendaController : ApiController
+    {
+        //
+        // GET: /Encomendas/
+
+        public IEnumerable<Encomenda> Get()
+        {
+            return IntegrationPri.ListaEncomendasPendentes();
+        }
+    }
+}
---
+++ SINF_CALIDRIS_VS/SINF_EXAMPLE_WS/Models/IntegrationPri.cs
@@ -0,0 +1,80 @@
+﻿using Interop.StdBE800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SINF_EXAMPLE_WS.Models
+{
+    public class IntegrationPri
+    {
+
+
+        public static List<Cliente> ListaClientes()
+        {
+
+
+            StdBELista objList;
+
+            List<Cliente> listClientes = new List<Cliente>();
+
+            if (PriEngine.InitializeCompany(SINF_EXAMPLE_WS.Properties.Settings.Default.Company.Trim(), SINF_EXAMPLE_WS.Properties.Settings.Default.User.Trim(), SINF_EXAMPLE_WS.Properties.Settings.Default.Password.Trim()) == true)
+            {
+
+                objList = PriEngine.Engine.Consulta("SELECT Cliente, Nome, Moeda, NumContrib as NumContribuinte, Fac_Mor AS campo_exemplo FROM  CLIENTES");
+
+                while (!objList.NoFim())
+                {
+                    listClientes.Add(new Cliente
+                    {
+                        CodCliente = objList.Valor("Cliente"),
+                        NomeCliente = objList.Valor("Nome"),
+                        Moeda = objList.Valor("Moeda"),
+                        NumContribuinte = objList.Valor("NumContribuinte"),
+                        Morada = objList.Valor("campo_exemplo")
+                    });
+                    objList.Seguinte();
+
+                }
+
+                return listClientes;
+            }
+            else
+                return null;
+        }
+
+
+        public static List<Encomenda> ListaEncomendasPendentes()
+        {
+
+
+            StdBELista objList;
+
+            List<Encomenda> listEncomendas = new List<Encomenda>();
+
+            if (PriEngine.InitializeCompany(SINF_EXAMPLE_WS.Properties.Settings.Default.Company.Trim(), SINF_EXAMPLE_WS.Properties.Settings.Default.User.Trim(), SINF_EXAMPLE_WS.Properties.Settings.Default.Password.Trim()) == true)
+            {
+
+                objList = PriEngine.Engine.Consulta("SELECT Ref, PrazoPicking, DataOrder FROM  CLIENTES WHERE Status = false orderBy Prioridade, PrazoPicking DESC");
+
+                while (!objList.NoFim())
+                {
+                    listEncomendas.Add(new Encomenda
+                    {
+                        Ref = objList.Valor("Ref"),
+                        PrazoPicking = objList.Valor("PrazoPicking"),
+                        DataOrder = objList.Valor("DataOrder")
+                    });
+                    objList.Seguinte();
+
+                }
+
+                return listEncomendas;
+            }
+            else
+                return null;
+        }
+    
+    
+    }
+}
